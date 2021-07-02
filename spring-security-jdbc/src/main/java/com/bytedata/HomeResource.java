6aa527a2bcacf458fd0413f48673b1ef2e868e54
bytedata <krishsg7@gmail.com>
2021-07-02 17:08:36 +0530

    Committing jdbcAuthentication

---
+++ spring-security-jdbc/src/main/java/com/bytedata/HomeResource.java
@@ -0,0 +1,22 @@
+package com.bytedata;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	@RequestMapping("/")
+	public String home() {
+		return ("<h1>Welcome home</h1>");
+	}
+
+	@RequestMapping("user")
+	public String user() {
+		return ("<h1>Welcome User</h1>");
+	}
+
+	@RequestMapping("/admin")
+	public String admin() {
+		return ("<h1>Welcome Admin</h1>");
+	}
+}
